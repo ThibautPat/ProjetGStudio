96da8175bf62e7aaf1c653bf4363515ee7212860
Diamond-R3D <jboulanger@gaming.tech>
2025-03-10 17:16:02 +0100

    jump done

--- src/LightEngine/SampleScene.cpp
+++ src/LightEngine/SampleScene.cpp
@@ -19,29 +19,29 @@ void SampleScene::TrySetSelectedEntity(DummyEntity* pEntity, int x, int y){}
 
 void SampleScene::OnUpdate()
 {
+	Jump();
+	sf::Vector2f move = Movement();
+}
+
+sf::Vector2f SampleScene::Movement()
+{
 	float x = 0;
 	float y = 0;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q) || sf::Joystick::getAxisPosition(0, sf::Joystick::X) < -10)
 	{
-		x = -100; 
+		x = -100;
 	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Joystick::getAxisPosition(0, sf::Joystick::X) > 10)
 	{
 		x = 100;
 	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z) || sf::Joystick::getAxisPosition(0, sf::Joystick::Y) < -10)
 	{
 		y = -100;
 	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Joystick::getAxisPosition(0, sf::Joystick::Y) > 10)
 	{
 		y = 100;
-	}
-
-	if (sf::Joystick::isConnected(0))
-	{
-		x = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
-		y = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
 	}
 
 	if (x > -10 && x < 10)
@@ -56,5 +56,22 @@ void SampleScene::OnUpdate()
 		y = -1;
 	if (y > 0)
 		y = 1;
-	std::cout << x << "," << y << std::endl;
+	
+	return sf::Vector2f(x , y);
+}
+
+void SampleScene::Jump()
+{
+	dt = jumpclock.getElapsedTime().asSeconds();
+	jumpclock.restart();
+	time += dt;
+	if (time > 1) 
+	{
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) || sf::Joystick::isButtonPressed(0, 0))
+		{
+			std::cout << "Jump" << std::endl;
+			time = 0;
+
+		}
+	}
 }
--- src/LightEngine/SampleScene.h
+++ src/LightEngine/SampleScene.h
@@ -1,23 +1,26 @@
 #pragma once
 
 #include "Scene.h"
-
+#include "SFML/System/Clock.hpp"
 class DummyEntity;
 
 class SampleScene : public Scene
 {
-	DummyEntity* pEntity1;
-	DummyEntity* pEntity2;
 
-	DummyEntity* pEntitySelected;
+	
 
 private:
 	void TrySetSelectedEntity(DummyEntity* pEntity, int x, int y);
 
 public:
+	float dt;
+	float time;
+	sf::Clock jumpclock;
 	void OnInitialize() override;
 	void OnEvent(const sf::Event& event) override;
 	void OnUpdate() override;
+	sf::Vector2f Movement();
+	void Jump();
 };
 
 
