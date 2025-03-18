4fba5cda3a663d4966e0cc97c030073fec1faea7
Diamond-R3D <jboulanger@gaming.tech>
2025-03-18 16:11:50 +0100

    merge tp / all w

--- src/LightEngine/GameScene/TestScene.cpp
+++ src/LightEngine/GameScene/TestScene.cpp
@@ -7,6 +7,7 @@
 #include "../GameEntity/Checkpoint.h"
 #include "../GameEntity/DeadlyObstacle.h"
 #include "../GameEntity/BackGround.h"
+#include "../GameEntity/Teleporter.h"
 
 //TODO in player class ----------
 void TestScene::PlayerDeath()
@@ -47,25 +48,7 @@ void TestScene::OnInitialize()
 	mView = new sf::View(sf::FloatRect(0, -340, GetWindowWidth()+56.25f, GetWindowHeight()+100)); // Ajout de la cam�ra
 	m_InstanceGameManager = GameManager::Get();
 
-	Teleporter* Teleporter1 = CreateRectEntity<Teleporter>(100, 100, sf::Color::Magenta); // Ajout du Teleporter et setup
-	Teleporter1->SetPosition(1200, 225);
-	Teleporter1->SetRigidBody(false);
-	Teleporter1->SetIsKinematic(true);
-	Teleporter1->SetGravity(false);
-	Teleporter1->SetTag(Tag::TELEPORTER);
-
-	Teleporter* Teleporter2 = CreateRectEntity<Teleporter>(100, 100, sf::Color::Magenta); // Ajout du Teleporter et setup
-	Teleporter2->SetPosition(700, 670);
-	Teleporter2->SetRigidBody(false);
-	Teleporter2->SetIsKinematic(true);
-	Teleporter2->SetGravity(false);
-	Teleporter2->SetTag(Tag::TELEPORTER);
-
-	Checkpoint* Checkpoint2 = CreateRectEntity<Checkpoint>(100, 100, sf::Color::Yellow); // Ajout du Checkpoint et setup
-	Checkpoint2->SetPosition(300, 670);
-	Checkpoint2->SetRigidBody(false);
-	Checkpoint2->SetIsKinematic(true);
-	Checkpoint2->SetGravity(false);
+	
 
 	BackGround* pEntity7 = CreateRectEntity<BackGround>(1090, 3350, sf::Color::White);
 	pEntity7->SetPosition(0, 205); 
@@ -119,12 +102,12 @@ void TestScene::OnInitialize()
 	pEntity4->SetGravity(false);
 	pEntity4->SetTag(Tag::OBSTACLE);
 
-	RectangleEntity* pEntity6 = CreateRectEntity<RectangleEntity>(100, 100, sf::Color::White);
-	pEntity6->SetPosition(-400, 0);
-	pEntity6->SetRigidBody(true);
-	pEntity6->SetIsKinematic(true);
-	pEntity6->SetGravity(false); 
-	pEntity6->SetTag(Tag::BOUCING_OBSTACLE);
+	//RectangleEntity* pEntity6 = CreateRectEntity<RectangleEntity>(100, 100, sf::Color::White);
+	//pEntity6->SetPosition(1200, 200);
+	//pEntity6->SetRigidBody(true);
+	//pEntity6->SetIsKinematic(true);
+	//pEntity6->SetGravity(false); 
+	//pEntity6->SetTag(Tag::BOUCING_OBSTACLE);
 
 	RectangleEntity* pEntity2 = CreateRectEntity<RectangleEntity>(100, 100, sf::Color::Green);
 	pEntity2->SetPosition(100, 0);
@@ -132,6 +115,26 @@ void TestScene::OnInitialize()
 	pEntity2->SetIsKinematic(true);
 	pEntity2->SetGravity(false);
 	pEntity2->SetTag(Tag::METALIC_OBSTACLE);
+
+	Teleporter* Teleporter1 = CreateRectEntity<Teleporter>(100, 100, sf::Color::Magenta); // Ajout du Teleporter et setup
+	Teleporter1->SetPosition(1200, -200);
+	Teleporter1->SetRigidBody(false);
+	Teleporter1->SetIsKinematic(true);
+	Teleporter1->SetGravity(false);
+	Teleporter1->SetTag(Tag::TELEPORTER);
+
+	Teleporter* Teleporter2 = CreateRectEntity<Teleporter>(100, 100, sf::Color::Magenta); // Ajout du Teleporter et setup
+	Teleporter2->SetPosition(1200, 600);
+	Teleporter2->SetRigidBody(false);
+	Teleporter2->SetIsKinematic(true);
+	Teleporter2->SetGravity(false);
+	Teleporter2->SetTag(Tag::TELEPORTER);
+
+	Checkpoint* Checkpoint2 = CreateRectEntity<Checkpoint>(100, 100, sf::Color::Yellow); // Ajout du Checkpoint et setup
+	Checkpoint2->SetPosition(300, 670);
+	Checkpoint2->SetRigidBody(false);
+	Checkpoint2->SetIsKinematic(true);
+	Checkpoint2->SetGravity(false);
 
 }
 
@@ -192,21 +195,23 @@ void TestScene::OnUpdate()
 				if (dynamic_cast<BackGround*>(entity2))
 				{
 
-					if (entity2->IsTag(Tag::BACK_GROUND1)&& !screenTouched) 
-						entity2->SetPosition(mView->getCenter().x - entity->GetPosition(0,0).x/8 -100, 205);
+					if (entity2->IsTag(Tag::BACK_GROUND1) && !screenTouched)
+						entity2->SetPosition(mView->getCenter().x - entity->GetPosition(0, 0).x / 8 - 100, 205);
 					for (Entity* entity3 : m_InstanceGameManager->GetEntities<Entity>()) // Parcours des entit�s du gameManager
 					{
 						if (entity2->IsTag(Tag::BACK_GROUND2BIS) && entity->GetPosition(0, 0).x > entity3->GetPosition(0, 0).x && entity3->IsTag(Tag::BACK_GROUND2) && !screenTouched)
-							entity2->SetPosition(mView->getCenter().x - entity->GetPosition(0, 0).x + entity2->GetShape()->getGlobalBounds().width*1.227f, 205);
+							entity2->SetPosition(mView->getCenter().x - entity->GetPosition(0, 0).x + entity2->GetShape()->getGlobalBounds().width * 1.227f, 205);
 						else if (entity2->IsTag(Tag::BACK_GROUND2BIS) && entity->GetPosition(0, 0).x < entity3->GetPosition(0, 0).x && entity3->IsTag(Tag::BACK_GROUND2) && !screenTouched)
-							entity2->SetPosition(mView->getCenter().x - entity->GetPosition(0, 0).x - entity2->GetShape()->getGlobalBounds().width/1.294f, 205);}}
+							entity2->SetPosition(mView->getCenter().x - entity->GetPosition(0, 0).x - entity2->GetShape()->getGlobalBounds().width / 1.294f, 205);
+					}
+				}
 				if (entity2->IsTag(Tag::TELEPORTER))
 				{
 					if (entity->GetShape()->getGlobalBounds().intersects(entity2->GetShape()->getGlobalBounds())) // Si le joueur touche le teleporter
 					{
 						for (Entity* entity3 : m_InstanceGameManager->GetEntities<Entity>()) // Parcours des entit�s du gameManager
 						{
-							if (entity3->IsTag(Tag::TELEPORTER) && entity3 != entity2 && TeleportClock.getElapsedTime().asSeconds() > 2) // Si l'entit� est un autre teleporter
+							if (entity3->IsTag(Tag::TELEPORTER) && entity3 != entity2 && TeleportClock.getElapsedTime().asSeconds() > 0.6f) // Si l'entit� est un autre teleporter
 							{
 								entity->SetPosition(entity3->GetPosition(0.f, 0.f).x, entity3->GetPosition(0.f, 0.f).y); // On t�l�porte le joueur
 								TeleportClock.restart(); // On restart le timer de t�l�portation
