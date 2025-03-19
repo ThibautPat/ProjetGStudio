28b37513ca1da7ed6e311621446176c2b9e848eb
ThibautPat <tpatry@gaming.tech>
2025-03-19 09:34:52 +0100

    cleanup

--- src/LightEngine/Core/TextureRender.cpp
+++
@@ -1,9 +0,0 @@
-#include "TextureRender.h"
-
-void TextureRender::SetupTexture(const char* path, const char* textName)
-{
-	TextureManager* assetsManager = GameManager::Get()->GetAssetsManager();
-	mTextName = textName;
-	assetsManager->Load(path, mTextName);
-	assetsManager->Find(mTextName, this);
-}
